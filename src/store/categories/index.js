4b9b70d80c5c29a711bbe79eacc3b7e25440e082
KaedenOC <komeacarr2@gmail.com>
2023-07-06 16:11:13 -0700

    categories store work

--- src/store/categories/index.js
+++ src/store/categories/index.js
@@ -3,11 +3,34 @@ let initialState = {
     { name: 'electronics', displayName: 'Electronics', description: 'definitely electronics' },
     { name: 'food', displayName: 'Food', description: 'definitely food' },
     { name: 'clothing', displayName: 'Clothing', description: 'definitely clothes' },
-  ]
+  ],
+  activeCategory: '',
 };
 
-function categoriesReducer(state=initialState, action) {
-  
+function categoriesReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'CHANGE_CATEGORY':
+      return {
+        ...state,
+        categories: state.categories.map(category => {
+          if (category.name === action.payload.name) { //payload is a property of action
+            return {
+              name: category.name,
+              displayName: category.displayName,
+              activeCategory: category.name,
+            }
+          }
+          return category;
+        })
+      }
+  }
+}
+
+export const changeCategory = (category) => {
+  return{
+    type: 'CHANGE_CATEGORY',
+    payload: category,
+  }
 }
 
 export default categoriesReducer;
