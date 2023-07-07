fdeec385137da8b73095509dc78494221a8ae16f
KaedenOC <komeacarr2@gmail.com>
2023-07-07 11:07:48 -0700

    products rendering on category click

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -5,7 +5,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useSelector } from 'react-redux';
 import { When } from 'react-if';
-import { CardMedia } from '@mui/material';
+import { CardMedia, Grid } from '@mui/material';
 
 export default function Products() {
 
@@ -17,30 +17,33 @@ export default function Products() {
       <When condition={activeCategory}>
         <h2>{activeCategory.displayName}</h2>
         <h4>Category Description Goes Here</h4>
-        {
-          products.map((product, index) => (
-            <Card key={`products${index}`} sx={{ maxWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image={`https://source.unsplash.com/random?${product.name}`}
-                title={product.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {product.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {product.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Add To Cart</Button>
-                <Button size="small">View Details</Button>
-              </CardActions>
-            </Card>
-            // <p key={`products${index}`}>{product.name}</p>
-          ))
-        }
+        <Grid container spacing={2} width="70%" margin="auto">
+          {
+            products.map((product, index) => (
+              <Grid key={`products${index}`} item xs={12} md={6} lg={4}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image={`https://source.unsplash.com/random?${product.name}`}
+                    title={product.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {product.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {product.description}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Add To Cart</Button>
+                    <Button size="small">View Details</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))
+          }
+        </Grid>
       </When>
     </>
   );
