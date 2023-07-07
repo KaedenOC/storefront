35827bb6a31b25a25ebc62104e86db01b575762e
KaedenOC <komeacarr2@gmail.com>
2023-07-06 17:19:29 -0700

    updating README

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import changeCategory from '../../store/categories';
+import { changeCategory } from '../../store/categories';
 
 function Categories() {
   const { categories } = useSelector((state) => state.categories);
